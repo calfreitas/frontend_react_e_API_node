c63e531ca883b457feb29f52acb6f966a49315ef
calliel.freitas <callieldefreitas@gmail.com>
2024-09-09 22:23:11 -0300

    adding axios / / API connection

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,41 +1,48 @@
 import "./style.css";
 import Trash from "../../assets/16qg.svg";
+import api from '../../services/api';
 
 //trabalhando com dados mocados simulando resultados >> API
 
-const users = [
-  {
-    id: "test@teste",
-    name: "kdjaksd",
-    age: "33", //idade definida como string no inicio do projeto da API
-    email: "teste@teste.com",
-  },
-  {
-    id: "test@teste2",
-    name: "kdjaksd2",
-    age: "34", //idade definida como string no inicio do projeto da API
-    email: "teste2@teste2.com",
-  },
-];
+// const users = [
+//   {
+//     id: "test@teste",
+//     name: "kdjaksd",
+//     age: "33", //idade definida como string no inicio do projeto da API
+//     email: "teste@teste.com",
+//   },
+//   {
+//     id: "test@teste2",
+//     name: "kdjaksd2",
+//     age: "34", //idade definida como string no inicio do projeto da API
+//     email: "teste2@teste2.com",
+//   },
+// ];
 
 function Home() {
+
+  const users = []
+
+  async function getUsers() {
+  }
+
   return (
     <>
       <div className="container">
         <form>
           <h1>Cadastro de usuários</h1>
-          <input name="nome" type="text" />
-          <input name="idade" type="number" />
-          <input name="email" type="email" />
+          <input placeholder="Nome" name="nome" type="text" />
+          <input placeholder="Idade" name="idade" type="text" />
+          <input placeholder="Email" name="email" type="email" />
           <button type="button">Cadastrar</button>
         </form>
 
         {users.map((user) => (
-          <div key={user.id}>
+          <div key={user.id} className="card">
             <div>
-              <p>Nome: {user.name} </p>
-              <p>Idade: {user.age} </p>
-              <p>Email: {user.email} </p>
+              <p>Nome: <span>{user.name}</span> </p>
+              <p>Idade: <span>{user.age}</span> </p>
+              <p>Email: <span>{user.email}</span> </p>
             </div>
             <button>
               <img src={Trash} />
